e36211eae10ae44d39466b1afaf83b7ec36abb6d
kkw2238 <kkw2238@naver.com>
2024-10-28 17:23:00 +0900

    init: 초기 설정
    toss 예제 내용으로 초기화

---
+++ src/test/java/com/sparta/nugulpayment/NugulPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nugulpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NugulPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

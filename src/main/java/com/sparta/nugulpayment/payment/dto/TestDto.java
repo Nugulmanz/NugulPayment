afc0b64ba68ffc2618673696410bbcaac12ef441
Juho <ljlh9509@gmail.com>
2024-11-12 23:35:20 +0900

    feat: SQS 테스트 API

--- src/main/java/com/sparta/nugulpayment/payment/dto/TestDto.java
+++ src/main/java/com/sparta/nugulpayment/payment/dto/TestDto.java
@@ -2,7 +2,11 @@ package com.sparta.nugulpayment.payment.dto;
 
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public class TestDto {
-    private Long id;
+    private String type;
+    private Object object;
+    private Map<String, Object> data;  // data를 Map으로 설정
 }
--- src/main/java/com/sparta/nugulpayment/payment/service/PaymentService.java
+++ src/main/java/com/sparta/nugulpayment/payment/service/PaymentService.java
@@ -13,6 +13,32 @@ public class PaymentService {
      */
     @SqsListener(value = "${cloud.aws.sqs.queue.name}")
     public void receiveMessage(TestDto testDto) {
-        System.out.println("Received message: " + testDto.getId());
+        System.out.println("Received type: " + testDto.getType());
+        System.out.println("Received object: " + testDto.getObject());
+        System.out.println("Received data: " + testDto.getData());
+
+        System.out.println("data containsKey false orderId : "+ testDto.getData().containsKey("orderId")); // true or false
+        System.out.println("data containsKey paymentKey : "+ testDto.getData().containsKey("paymentKey"));
+        System.out.println("data containsKey amount : "+ testDto.getData().containsKey("amount"));
+
+        System.out.println("data containsKey false orderId : "+ testDto.getData().get("orderId")); // value
+        System.out.println("data containsKey paymentKey : "+ testDto.getData().get("paymentKey"));
+        System.out.println("data containsKey amount : "+ testDto.getData().get("amount"));
+
+
+
+//        switch (testDto.getType()) {
+//            case "transfer":
+//                System.out.println("Received type: " + testDto.getType());
+//                testDto.getData() 이거 안에 내용 분리해서 사용할 수 있게 정제하기
+//
+//                if(testDto.getData().getOderId==false){
+//                    에러 날리기
+//                }
+//                아니면 로직 쭉 나가기
+//            case "reserve":
+//                System.out.println("Received type: " + testDto.getType());
+//
+//        }
     }
 }
